56efdebca8e4ef82166067defcde7a995bd3f72b
milad-emami <milad.emami1025@gmail.com>
2023-04-15 16:42:54 +0330

    fix: resolve undefined reguest

--- src/hooks/useLogo.jsx
+++ src/hooks/useLogo.jsx
@@ -1,18 +1,20 @@
-import { AppContext } from "@edx/frontend-platform/react";
+import { AppContext } from '@edx/frontend-platform/react';
 
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from 'react';
 
 export default function useLogo() {
-  const [data, setData] = useState(undefined);
+  const [data, setData] = useState();
 
   const { config } = useContext(AppContext);
   useEffect(() => {
-    fetch(`${config.LMS_BASE_URL}${config.AC_INSTANCE_CONFIG_API_URL}`)
-      .then((response) => response.json())
-      .then((response) => setData(JSON.parse(response)))
-      .catch((err) => {
-        console.log(err);
-      });
+    if (config.AC_INSTANCE_CONFIG_API_URL && config.LMS_BASE_URL) {
+      fetch(`${config.LMS_BASE_URL}${config.AC_INSTANCE_CONFIG_API_URL}`)
+        .then((response) => response.json())
+        .then((response) => setData(JSON.parse(response)))
+        .catch((err) => {
+          console.log(err);
+        });
+    }
   }, [config.AC_INSTANCE_CONFIG_API_URL, config.LMS_BASE_URL]);
   return `${data?.logo ?? data?.edspirit_logo}`;
 }
