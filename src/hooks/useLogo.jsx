fd393d52319601c6af4c5483f611e56fca6c6ae7
milad-emami <milad.emami1025@gmail.com>
2023-07-03 16:57:18 +0330

    fix: logo place holder

--- src/hooks/useLogo.jsx
+++ src/hooks/useLogo.jsx
@@ -3,18 +3,24 @@ import { useContext, useEffect, useState } from 'react';
 import logoPlaceholder from '../assets/org-logo-place-holder.svg';
 
 export default function useLogo() {
-  const [data, setData] = useState();
- const { config } = useContext(AppContext);
+  const [data, setData] = useState({});
+  const { config } = useContext(AppContext);
+
   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${config.LMS_BASE_URL}${config.AC_INSTANCE_CONFIG_API_URL}`);
+        const result = await response.json();
+        setData(JSON.parse(result));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
     if (config.AC_INSTANCE_CONFIG_API_URL && config.LMS_BASE_URL) {
-      fetch(`${config.LMS_BASE_URL}${config.AC_INSTANCE_CONFIG_API_URL}`)
-        .then((response) => response.json())
-        .then((response) => setData(JSON.parse(response)))
-        .catch((err) => {
-          console.log(err);
-        });
+      fetchData();
     }
   }, [config.AC_INSTANCE_CONFIG_API_URL, config.LMS_BASE_URL]);
 
-  return `${data?.logo ?? (data?.edspirit_logo || logoPlaceholder)}`;
+  return data?.logo || logoPlaceholder;
 }
