1eec46ce363761f78306820a5cd9d9d6c5c5be84
milad-emami <milad.emami1025@gmail.com>
2023-07-22 16:12:07 +0330

    fix: add react query

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,15 +4,29 @@ import DesktopHeader from './Header/DesktopHeader';
 import MobileHeader from './Header/MobileHeader';
 import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
 import SearchModal from './Header/SearchModal';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // Set staleTime to 5 minutes
+      staleTime: 5 * 60 * 1000,
+      // Set cacheTime to 60 minutes
+      cacheTime: 60 * 60 * 1000,
+    },
+  },
+});
 const Header = () => {
   const isMobile = useMediaQuery({ maxWidth: '768px' });
   const [isOpen, open, close] = useToggle(false);
   return (
-    <header>
-      <SearchModal isOpen={isOpen} onClose={close} />
-      {isMobile ? <MobileHeader open={open} /> : <DesktopHeader />}
-    </header>
+    <QueryClientProvider client={queryClient}>
+      <header>
+        <SearchModal isOpen={isOpen} onClose={close} />
+        {isMobile ? <MobileHeader open={open} /> : <DesktopHeader />}
+      </header>
+    </QueryClientProvider>
   );
 };
 
--- src/Header/useGetUserProfile.js
+++ src/Header/useGetUserProfile.js
@@ -1,44 +1,30 @@
-import { getConfig } from "@edx/frontend-platform";
-import { useContext, useState, useEffect } from "react";
-import { AppContext } from "@edx/frontend-platform/react";
-import { getAuthenticatedHttpClient } from "@edx/frontend-platform/auth";
+import { getConfig } from '@edx/frontend-platform';
+import { useQuery } from 'react-query';
+import { useContext } from 'react';
+import { AppContext } from '@edx/frontend-platform/react';
+import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
 
 const useGetUserProfile = () => {
   const { authenticatedUser } = useContext(AppContext);
-  const [userProfile, setUserProfile] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const fetchUserProfile = async () => {
-      setLoading(true);
-      try {
-        const { data, status } = await getAuthenticatedHttpClient().get(
-          `${getConfig().LMS_BASE_URL}/api/user/v1/accounts/${
-            authenticatedUser?.username
-          }`
-        );
-        if (status !== 200) {
-          throw new Error("user fetch not ok");
-        }
-        setUserProfile(data);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    if (authenticatedUser) {
-      fetchUserProfile();
-    } else {
-      setUserProfile(null);
+  console.log('test-react-query');
+  const fetchUserProfile = async () => {
+    const { data, status } = await getAuthenticatedHttpClient().get(
+      `${getConfig().LMS_BASE_URL}/api/user/v1/accounts/${
+        authenticatedUser?.username
+      }`
+    );
+    if (status !== 200) {
+      throw new Error('user fetch not ok');
     }
-  }, [authenticatedUser]);
+    return data;
+  };
 
+  const { isLoading, data } = useQuery('fetchUserProfile', fetchUserProfile, {
+    enabled: !!authenticatedUser,
+  });
   return {
-    userProfile,
-    loading,
+    userProfile: data,
+    loading: isLoading,
   };
 };
-
 export default useGetUserProfile;
