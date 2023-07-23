7deb794a45c712d38f0debfd546e7fd2e4705a27
milad-emami <milad.emami1025@gmail.com>
2023-07-23 16:35:48 +0330

    feat: add new header

---
+++ src/Header/useGetConfig.js
@@ -0,0 +1,31 @@
+import { getConfig } from '@edx/frontend-platform';
+import { useQuery } from 'react-query';
+import logoPlaceholder from '../assets/org-logo-place-holder.svg';
+
+const useGetConfig = () => {
+  const fetchConfig = async ({ baseURL, instanceConfigAPIUrl }) => {
+    const response = await fetch(`${baseURL}${instanceConfigAPIUrl}`);
+    const result = await response.json();
+    return JSON.parse(result)?.logo;
+  };
+
+  const { data, isLoading, isError } = useQuery(
+    'headerLogo',
+    () =>
+      fetchConfig({
+        baseURL: getConfig().LMS_BASE_URL,
+        instanceConfigAPIUrl: getConfig().AC_INSTANCE_CONFIG_API_URL,
+      }),
+    {
+      enabled:
+        !!getConfig().LMS_BASE_URL && !!getConfig().AC_INSTANCE_CONFIG_API_URL,
+    }
+  );
+
+  return {
+    headerLogo: data || logoPlaceholder,
+    loading: isLoading,
+    isError,
+  };
+};
+export default useGetConfig;
