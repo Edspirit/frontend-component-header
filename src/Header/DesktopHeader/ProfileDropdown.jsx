d65d6471934da332b0ab63e736d1c980c8e3cda3
milad-emami <milad.emami1025@gmail.com>
2023-09-30 14:46:11 +0330

    fix: fix order history url

--- src/Header/DesktopHeader/ProfileDropdown.jsx
+++ src/Header/DesktopHeader/ProfileDropdown.jsx
@@ -36,41 +36,44 @@ const ProfileDropdown = () => {
       <Dropdown.Toggle iconAfter={ArrowDropDown} iconBefore={renderAvatar} />
       <Dropdown.Menu alignRight>
         <Dropdown.Item
-          href={`${getConfig().ACCOUNT_PROFILE_URL}/u/${authenticatedUser?.username}`}
+          href={`${getConfig().ACCOUNT_PROFILE_URL}/u/${
+            authenticatedUser?.username
+          }`}
         >
           <FormattedMessage
             id="header.dropdownOption.profile"
             defaultMessage="Profile"
           />
         </Dropdown.Item>
-        <Dropdown.Item
-          href={getConfig().ACCOUNT_SETTINGS_URL}
-        >
+        <Dropdown.Item href={getConfig().ACCOUNT_SETTINGS_URL}>
           <FormattedMessage
             id="header.dropdownOption.account"
             defaultMessage="Account"
           />
         </Dropdown.Item>
         <Dropdown.Item
-          href={`https://${getConfig().BASE_URL.replace('apps.', '')}/homepage/overview`}
+          href={`https://${getConfig().BASE_URL.replace(
+            'apps.',
+            '',
+          )}/homepage/overview`}
         >
           <FormattedMessage
             id="header.dropdownOption.dashboard"
             defaultMessage="Dashboard"
           />
         </Dropdown.Item>
-        { hasBilling && (
-        <Dropdown.Item
-          href={`https://billing.${getConfig().LMS_BASE_URL.replace(
-            'https://',
-            '',
-          )}`}
-        >
-          <FormattedMessage
-            id="header.dropdownOption.orderHistory"
-            defaultMessage="Order History"
-          />
-        </Dropdown.Item>
+        {hasBilling && (
+          <Dropdown.Item
+            href={`https://billing.${getConfig().BASE_URL.replace(
+              'apps.',
+              '',
+            )}`}
+          >
+            <FormattedMessage
+              id="header.dropdownOption.orderHistory"
+              defaultMessage="Order History"
+            />
+          </Dropdown.Item>
         )}
         <Dropdown.Item onClick={handleLogout}>
           <FormattedMessage
