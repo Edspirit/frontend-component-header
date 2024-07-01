d237fc7422765309d70966535b40702cc8c94604
milad-emami <milad.emami1025@gmail.com>
2024-07-01 13:31:00 +0330

    feat: update header to new version

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,22 +1,24 @@
 import arMessages from './messages/ar.json';
 import faIRMessages from './messages/fa_IR.json';
-import frMessages from './messages/fr.json';
+// no need to import en messages-- they are in the defaultMessage field
 import es419Messages from './messages/es_419.json';
+import frMessages from './messages/fr.json';
 import zhcnMessages from './messages/zh_CN.json';
 import zhhkMessages from './messages/zh_HK.json';
 import ruMessages from './messages/ru.json';
 import ukMessages from './messages/uk.json';
 import ptMessages from './messages/pt.json';
 import itMessages from './messages/it.json';
-import ukMessages from './messages/uk.json';
 import deMessages from './messages/de.json';
-import ruMessages from './messages/ru.json';
 import hiMessages from './messages/hi.json';
-import frCAMessages from './messages/fr_CA.json';
-// no need to import en messages-- they are in the defaultMessage field
+import frcaMessages from './messages/fr_CA.json';
+import ititMessages from './messages/it_IT.json';
+import ptptMessages from './messages/pt_PT.json';
+import dedeMessages from './messages/de_DE.json';
 
 const messages = {
   ar: arMessages,
+  'fa-ir': faIRMessages,
   fa: faIRMessages,
   'es-419': es419Messages,
   fr: frMessages,
@@ -28,9 +30,10 @@ const messages = {
   it: itMessages,
   de: deMessages,
   hi: hiMessages,
-  'fr-ca': frCAMessages,
-  ru: ruMessages,
-  uk: ukMessages,
+  'fr-ca': frcaMessages,
+  'it-it': ititMessages,
+  'pt-pt': ptptMessages,
+  'de-de': dedeMessages,
 };
 
 export default messages;
