9769bc2b69b7012d6b511c507662617a32acc5d8
milad-emami <milad.emami1025@gmail.com>
2024-12-08 16:24:39 +0330

    fix: studio

--- src/studio-header/StudioHeader.jsx
+++ src/studio-header/StudioHeader.jsx
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types';
 import Responsive from 'react-responsive';
 import { AppContext } from '@edx/frontend-platform/react';
 import { ensureConfig } from '@edx/frontend-platform';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import MobileHeader from './MobileHeader';
 import HeaderBody from './HeaderBody';
+import Head from '../Header/Head';
 
 ensureConfig([
   'STUDIO_BASE_URL',
@@ -15,9 +17,19 @@ ensureConfig([
   'LOGO_URL',
 ], 'Studio Header component');
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // Set staleTime to 5 minutes
+      staleTime: 5 * 60 * 1000,
+      // Set cacheTime to 60 minutes
+      cacheTime: 60 * 60 * 1000,
+    },
+  },
+});
+
 const StudioHeader = ({
-  number, org, title, containerProps, isHiddenMainMenu, mainMenuDropdowns,
-  outlineLink, searchButtonAction, isNewHomePage,
+  number, org, title, containerProps, isHiddenMainMenu, mainMenuDropdowns, outlineLink, searchButtonAction, isNewHomePage, mfeTitle
 }) => {
   const { authenticatedUser, config } = useContext(AppContext);
   const props = {
@@ -30,24 +42,28 @@ const StudioHeader = ({
     username: authenticatedUser?.username,
     isAdmin: authenticatedUser?.administrator,
     authenticatedUserAvatar: authenticatedUser?.avatar,
-    studioBaseUrl: isNewHomePage ? '/home' : config.STUDIO_BASE_URL,
+    studioBaseUrl: config.STUDIO_BASE_URL,
     logoutUrl: config.LOGOUT_URL,
     isHiddenMainMenu,
     mainMenuDropdowns,
     outlineLink,
     searchButtonAction,
+    isNewHomePage,
   };
 
   return (
-    <div className="studio-header">
-      <a className="nav-skip sr-only sr-only-focusable" href="#main">Skip to content</a>
-      <Responsive maxWidth={841}>
-        <MobileHeader {...props} />
-      </Responsive>
-      <Responsive minWidth={842}>
-        <HeaderBody {...props} />
-      </Responsive>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Head mfeTitle={mfeTitle} />
+      <div className="studio-header">
+        <a className="nav-skip sr-only sr-only-focusable" href="#main">Skip to content</a>
+        <Responsive maxWidth={841}>
+          <MobileHeader {...props} />
+        </Responsive>
+        <Responsive minWidth={842}>
+          <HeaderBody {...props} />
+        </Responsive>
+      </div>
+    </QueryClientProvider>
   );
 };
 
@@ -68,6 +84,7 @@ StudioHeader.propTypes = {
   outlineLink: PropTypes.string,
   searchButtonAction: PropTypes.func,
   isNewHomePage: PropTypes.bool.isRequired,
+  mfeTitle: PropTypes.string,
 };
 
 StudioHeader.defaultProps = {
@@ -78,6 +95,7 @@ StudioHeader.defaultProps = {
   mainMenuDropdowns: [],
   outlineLink: null,
   searchButtonAction: null,
+  mfeTitle: null,
 };
 
 export default StudioHeader;
